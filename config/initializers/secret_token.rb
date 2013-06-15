517e7ab0c15356119c8ee98f84efaada6b94b361
Kaushik Tiwari <kkt2123@columbia.edu>
2013-06-14 21:32:29 -0400

    Intial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EverHealth::Application.config.secret_token = 'd6ed79ad2424ec594e9df58289db183f901d4999c7ebd453891002083bc4889398c4260acb7a7abd2dd523baec9daab0dd57c713ace5b77ac6bd879545a0f7c3'
